411535334aa8da72e518eb651a5cb9c5b11b9158
beatrizsimoeess <abass@iscte-iul.pt>
2025-11-07 19:16:52 +0000

    gente what

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,9 +7,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Gson gson = new Gson();
-<<<<<<< HEAD
-        
-=======
->>>>>>> 73a33f9fdc04553cfc3cbc3807f24a1b5aa3bd87
     }
 }
